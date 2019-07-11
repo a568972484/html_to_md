b89696aeb9faa60edf21ea7e38ad8e936434e3e0
小小咸鱼ywy <49986186+a568972484@users.noreply.github.com>
2019-07-11 15:48:51 +0800

    Update html_to_md.py

--- html_to_md.py
+++ html_to_md.py
@@ -1,13 +1,19 @@
 import re
 import requests
+from bs4 import BeautifulSoup
 
 
-
-
+headers = {}
 def url_to_md_txt(url):
     try:
+        url = f'https://www.cnblogs.com/{url}'
         response = requests.get(url)
-        a = re.findall('<div id="cnblogs_post_body" class="blogpost-body cnblogs-markdown">(.*?)</div><div id="MySignature"></div>',response.text, re.S)[0]
+        a = re.findall('<div id="cnblogs_post_body" class="blogpost-body cnblogs-markdown">(.*?)</div><div id="MySignature"></div>',response.text, re.S)
+        if not a:
+            response_dome = BeautifulSoup(response.text, 'html.parser')
+            response_dome_str = str(response_dome.div)
+            a = re.findall('<div class="postBody">(.*?)</div><div id="MySignature"></div>', response_dome_str, re.S)
+        a = a[0]
         a = re.sub('<h1.*?>', '# ', a)
         a = re.sub('<h2.*?>', '## ', a)
         a = re.sub('<h3.*?>', '### ', a)
@@ -40,7 +46,7 @@ def url_to_md_txt(url):
         a = re.sub('<ol>', '', a)
         a = re.sub('<div.*?>', '', a)
         a = re.sub('<em id="__mceDel">', '', a)
-        a = re.sub('<a.*?>', '', a)
+        # a = re.sub('<a.*?>', '', a)
         a = re.sub('<em id="__mceDel">', '', a)
         a = re.sub('&gt', '>', a)
         lis_a = a.split('\n')
@@ -72,6 +78,9 @@ def url_to_md_txt(url):
         return False
 
 if __name__ == '__main__':
-    url = inport'请输入你的要爬取网页的的url\n:'
-    print('内容如下')
-    print(url_to_md_txt(url))
+    url = input('\033[31;m请输入你的要爬取网页的的url\n'
+                '例如https://www.cnblogs.com/pythonywy/p/11146937.html\n'
+                '你只要输入pythonywy/p/11146937.html即可\n'
+                '请输入')
+    print('内容如下,复制绿色内容即可')
+    print(f'\033[32;m{url_to_md_txt(url)}')
