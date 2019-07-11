f84abbe8022ad2c85b203cd544074151a6bc2188
小小咸鱼ywy <49986186+a568972484@users.noreply.github.com>
2019-07-11 10:18:25 +0800

    Update conf.py
    已经更加对于<br\>的处理

--- conf.py
+++ conf.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 
 
 
+#通过博客首页获取所有随笔以及对随笔计数的一个字典
+#对于链接和标题的一个整合
 def count_url_lis_to_dict(func_1):
     def wrapper(*args,**kwargs):
         dic = dict()
@@ -30,6 +32,7 @@ def count_url_lis_to_dict(func_1):
         print(f'保存的json文件中的内容{dic}')
         return dic
     return wrapper
+
 @count_url_lis_to_dict
 def get_count_url_lis(url):
     lis = []
@@ -48,15 +51,24 @@ def get_count_url_lis(url):
         lis = set(lis)
         lis = list(lis)
         count_2 = len(lis)
+
         if count_1 == count_2:
-            return count_2,lis  
+            return count_2,lis  #博客的数据量,博客里面随笔的url
+
+
+#保存爬取的字典成 json格式
 def dump_json(dict,url):
     file_name = url.split('/')[-2]
-    if not os.path.exists('博客园随笔'):os.mkdir('博客园随笔') 
+
+    if not os.path.exists('博客园随笔'):os.mkdir('博客园随笔') #创建个放随笔的文件夹
+
     file_path = os.path.join('博客园随笔', f'{file_name}.json')
     with open(file_path,'w',encoding='utf8') as fw:
         json.dump(dict,fw)
         print('字典保存json文件成功')
+
+
+#读取字典且保存去除字典中count 这一栏
 def read_dic(url):
     file_name = url.split('/')[-2]
     file_path = os.path.join('博客园随笔', f'{file_name}.json')
@@ -69,6 +81,8 @@ def read_dic(url):
     except:
         print('没有爬取下来链接')
         return False
+
+#输入随笔的url转md格式文件
 def url_to_md_txt(url):
     try:
         response = requests.get(url)
@@ -76,13 +90,19 @@ def url_to_md_txt(url):
         response_dome_str = str(response_dome.div)
         data = re.findall('<div class="postBody">(.*?)</div><div id="MySignature"></div>', response_dome_str, re.S)
         a = data[0]
+
+        # 标题
         a = re.sub('<h1.*?>', '# ', a)
         a = re.sub('<h2.*?>', '## ', a)
         a = re.sub('<h3.*?>', '### ', a)
         a = re.sub('<h4.*?>', '#### ', a)
         a = re.sub('<h5.*?>', '##### ', a)
         a = re.sub('<h6.*?>', '###### ', a)
-        a = re.sub('<strong>|</strong>', '**', 
+
+        # 加粗
+        a = re.sub('<strong>|</strong>', '**', a)
+
+        # 三个点
         if '<pre class=' in a:
             a = re.sub('<pre class="', '```', a)
             a = re.sub('"><code>', '\n', a)
@@ -91,25 +111,53 @@ def url_to_md_txt(url):
         a = re.sub('<code>|</code>', '```', a)
         a = re.sub('<div class="cnblogs_code".*?>', '```python', a)
         a = re.sub('</div>', '```', a)
+
+        # - 这个标识
         a = re.sub('<li>', '- ', a)
+
+        # 空格
         a = re.sub('<em>|</em>', ' ', a)
+
+        # 表格
+
         a = re.sub('<td.*?>|</td>\n', '|', a)
         a = re.sub('<th.*?>|</th>\n', '|', a)
         a = re.sub('<tr.*?>|</tr>\n', '', a)
         a = re.sub('</tbody>|</table>|<table>|<tbody>', '', a)
         a = re.sub('\|\|', '|', a)
+
+
+        #换行
+        a = re.sub('<p.*?>', '', a)
+        a = re.sub('<br/>', '\n', a)
+
+        # 没用的标识
         a = re.sub('<p.*?>', '', a)
         a = re.sub('<span.*?>', '', a)
+        # a = re.sub('</.*?>', '', a) 这个先不去掉,为了后面表格时候弄
         a = re.sub('<pre>', '', a)
         a = re.sub('<ul>', '', a)
         a = re.sub('<ol>', '', a)
         a = re.sub('<div.*?>', '', a)
+        a = re.sub('<em id="__mceDel">', '', a)
+        a = re.sub('<a.*?>', '', a)
+        a = re.sub('<em id="__mceDel">', '', a)
+
+
+        #算法里面箭头
         a = re.sub('&gt', '>', a)
+
+        # 加超链接
         lis_a = a.split('\n')
+
+
+        # 字符串
         text = ''
         for data in lis_a:
             if not data:
                 continue
+
+            # 处理表格
             if data == '|':
                 data = '\n'
             if '</thead>' in data:
@@ -118,20 +166,38 @@ def url_to_md_txt(url):
                 lis_format = '|:-:' * lis_x_count
                 data = f'{data_txt}\n{lis_format}|'
             text += f'{data}\n'
+
+        # 删除标签末尾
         text = re.sub('</.*?>', '', text)
+        #加超链接末尾
         lis = text.split('\n')
         for index in range(len(lis)):
             if 'href=' in lis[index]:
                 lis[index] = f'{lis[index]}</a>'
+
+        #变回字符串
         new_text = ''
         for aaaa in lis:
             new_text += f'{aaaa}\n'
+
+
+        #上面全是转md
         return new_text
+
+    #可能博客不一样会存在见状性没有用我匹配的格式找到内容
     except :
         return False
+
+#保存文档
 def text_to_file(name,text):
+
+    #创建文件夹
     if not os.path.exists('博客园随笔md格式'): os.mkdir('博客园随笔md格式')
+
+    #创建文件路径
     file_path = os.path.join('博客园随笔md格式', f'{name}.md')
+
+    #保存
     try:
         with open(file_path,'w',encoding='utf8') as fw:
             fw.write(text)
@@ -141,14 +207,14 @@ def text_to_file(name,text):
 
 
 
-
+#可视化窗口写:
 action_msg ='''
 输入1.批量爬取博客园首页的所有随笔字典并保存json文件,且随笔全部转成md格式文件
 输入2.输入指定随笔url把随笔内容转成md并且保存
 输入Q.退出脚本
 '''
 
-
+#功能1
 def action_1():
     print('输入博客园首页的格式为\n'
           '例如https://www.cnblogs.com/pythonywy/')
@@ -173,6 +239,7 @@ def action_1():
         print('输入url有误或者没有随笔')
 
 
+#功能二
 def action_2():
     print('输入url\n'
           '例如https://www.cnblogs.com/pythonywy/p/11123051.html\n'
@@ -186,7 +253,7 @@ def action_2():
         print('输入url有误或者没有随笔')
 
 
-
+#主界面:
 def run():
     while True:
         print(action_msg)
@@ -198,3 +265,5 @@ def run():
         elif action_msg_chiose =='2':action_2()
 
 
+if __name__ == '__main__':
+    print(url_to_md_txt('https://www.cnblogs.com/jihongjun/p/8673315.html'))
